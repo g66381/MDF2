c5b54fb110f0463846a1c11ebf7dd4b6553c49a1
Jonathan Kay <johnnyakay@fullsail.edu>
2015-02-03 15:42:37 -0500

    Basic layout for first screen with user name logged.

--- MDF2_Project1/MDF2_Project1/ViewController.m
+++ MDF2_Project1/MDF2_Project1/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Accounts/Accounts.h>
 
 @interface ViewController ()
 
@@ -15,6 +16,32 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    if (accountStore != nil) {
+        
+        ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+        if (accountType != nil) {
+            [accountStore requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+                if (granted) {
+                    
+                    // User allowed account access
+                    NSArray *twitAcnts = [accountStore accountsWithAccountType:accountType];
+                    if (twitAcnts != nil) {
+                        ACAccount *curAcnt = [twitAcnts objectAtIndex:0];
+                        if (curAcnt != nil) {
+                            user.text = (curAcnt.username);
+                        }
+                    }
+                    
+                }
+                else {
+                    //User said no
+                }
+            }];
+        }
+    }
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
